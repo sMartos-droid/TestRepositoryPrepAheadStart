2a40ddc964573ac53f27bcf64fcbdf64790e0044
sMartos <sergio.martosmontoya@googlemail.com>
2026-08-08 17:30:47 +0200

    [SWQT] adding more fields to the employee class

--- srcFiles/employee.py
+++ srcFiles/employee.py
@@ -1,34 +1,32 @@
 from srcFiles.person import Person
 
-
 class Employee(Person):
-    def __init__(self, firstName, lastName, personalID, employeeID, grade=None, salary=None):
-        self.__employeeID = employeeID
-        self.__grade = grade
-        self.__salary = salary
-        super().__init__(firstName, lastName, personalID)
+        #employeeID and grade are private attributes
+        #grade values can be "junior", "senior", "manager", "director"
+        def __init__(self, firstName, lastName, personalID, employeeID, grade, salary):
+                self.__employeeID = employeeID
+                self.__grade = grade
+                self.__salary = salary
+                super().__init__(firstName, lastName, personalID)
+                
+        #getters
+        def getEmployeeID(self):
+                return self.__employeeID
+        
+        def getGrade(self):
+                return self.__grade
 
-    # getters
-    def getEmployeeID(self):
-        return self.__employeeID
+        def getSalary(self):
+                return self.__salary
 
-    def getGrade(self):
-        return self.__grade
+        #setters
+        def setEmployeeID(self, employeeID):
+                self.__employeeID = employeeID
 
-    def getSalary(self):
-        return self.__salary
-
-    # setters
-    def setEmployeeID(self, employeeID):
-        self.__employeeID = employeeID
-
-    def setGrade(self, grade):
-        self.__grade = grade
-
-    def setSalary(self, salary):
-        self.__salary = salary
-
-    # methods
-    def toString(self):
-        return super().toString() + " " + str(self.getEmployeeID())
+        def setGrade(self, grade):
+                self.__grade = grade
+                
+        #methods 
+        def toString(self):
+                return (super().toString() + " " + str(self.getEmployeeID()))
 
