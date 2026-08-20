2c3451be52688af9a6ad204747a6d19c14aa533d
sMartos <sergio.martosmontoya@googlemail.com>
2026-08-20 19:04:39 +0200

    [SW.6] enhancing bank class documentation

---
+++ srcFiles/bankPackage/dog.py
@@ -0,0 +1,32 @@
+""" This class models a dog"""
+class Dog:
+    def __init__(self, name, legs):
+        #inner fields (priavate with __)
+        self.__name = name
+        self.__nrOflegs = legs
+        
+    #getters
+    """
+    Returns the name of bank account owner
+        Parameters:
+            N/A
+        Returns:
+            (str): owner´s name
+    """
+    def getName(self):
+        return self.__name
+    def getNrOfLegs(self):
+        return self.__nrOflegs
+        
+    #setters
+    def setName(self, name):
+        self.__name = name
+    def setNrOfLegs(self, legs):
+        self.__nrOflegs = legs
+            
+    #methods
+    def seak(self):
+        print("guau gau")
+        
+        
+        
